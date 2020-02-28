972865e84c9aeaa388feb516d258fe5e81e0e7dc
grishberg <grishberg@ya.ru>
2020-02-28 09:29:35 +0300

    Initial commit

---
+++ app/src/main/java/com/github/grishberg/customlm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.grishberg.customlm;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
